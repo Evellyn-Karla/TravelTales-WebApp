f96932733605c560bcba3d1da7bdfb5dfa1e9dce
Evellyn KS <132318866+VellyKs@users.noreply.github.com>
2024-04-25 19:39:01 -0300

    Add files via upload

---
+++ public/pages/pesquisa.js
@@ -0,0 +1,21 @@
+function telaPerfil(){
+    window.location.href = 'van.html'
+}
+function telaSalvos(){
+    window.location.href = 'salvos.html'
+}
+function telaHome(){
+    window.location.href = '../../index.html'
+}
+
+function voltar() {
+    window.history.back();
+}
+
+
+
+document.getElementById('salvos').addEventListener('click', telaSalvos)
+document.getElementById('home').addEventListener('click', telaHome)
+document.getElementById('perfil').addEventListener('click', telaPerfil)
+document.getElementById('')
+
