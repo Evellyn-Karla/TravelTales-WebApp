9614dca4f28102f1b2d6ea14ebcbabe6c04b2405
Evellyn KS <132318866+VellyKs@users.noreply.github.com>
2024-04-25 16:59:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ window.addEventListener('load', function() {
     }
 });
 
+function telaPerfil(){
+    window.location.href = 'public/pages/van.html'
+}
+function telaSalvos(){
+    window.location.href = 'public/pages/van.html'
+}
+function telaPesquisa(){
+    window.location.href = 'public/pages/pesquisa.html'
+}
+
+document.getElementById('salvos').addEventListener('click', telaSalvos)
+document.getElementById('pesquisar').addEventListener('click', telaPesquisa)
+document.getElementById('Perfil').addEventListener('click', telaPerfil)
+
 
