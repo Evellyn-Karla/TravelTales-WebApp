e15388b6765f8d80447b9b4c06f7f2709825b09c
Evellyn KS <132318866+VellyKs@users.noreply.github.com>
2024-04-25 18:37:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ function telaPesquisa(){
     window.location.href = 'public/pages/pesquisa.html'
 }
 
+
+
 document.getElementById('salvos').addEventListener('click', telaSalvos)
+document.getElementById('buscar').addEventListener('click', telaPesquisa)
+document.getElementById('perfil').addEventListener('click', telaPerfil)
+
+
 document.getElementById('pesquisar').addEventListener('click', telaPesquisa)
-document.getElementById('Perfil').addEventListener('click', telaPerfil)
 
 
