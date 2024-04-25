6b53a8e6a1ba123a49d27917fbf119b5f3078e2f
Débora Ramos <115510517+itsdrf@users.noreply.github.com>
2024-04-25 20:46:27 -0300

    Update pesquisa.js

--- public/pages/pesquisa.js
+++ public/pages/pesquisa.js
@@ -1,3 +1,24 @@
+   function renderizarMapa(latitude, longitude) {
+            var map = L.map('map').setView([latitude, longitude], 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup('Você está aqui.')
+                .openPopup();
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                document.querySelector('p').innerHTML = "Latitude = " + position.coords.latitude + "<br>Longitude = " + position.coords.longitude;
+                renderizarMapa(position.coords.latitude, position.coords.longitude);
+            });
+        } else {
+            document.querySelector('p').innerHTML = "Geolocalização não suportada pelo seu navegador.";
+        }
+
 function telaPerfil(){
     window.location.href = 'van.html'
 }
