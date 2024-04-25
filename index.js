a7babd55a2abd46e0c948862d506e689732c2765
Evellyn KS <132318866+VellyKs@users.noreply.github.com>
2024-04-25 16:08:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function() {
     var usuarioCadastrado = localStorage.getItem('usuario');
     if (!usuarioCadastrado) {
         window.location.href = 'public/pages/login.html';
+    }else{
+    document.querySelector('body').style.display = "block';
     }
 });
 
